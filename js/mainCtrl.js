ceee271983cc6eec10ff8c2c3ebd0837b98d78d0
Ann Kim <aumk21@gmail.com>
2015-08-18 18:38:21 -0600

    Commit

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,4 +2,6 @@ var app = angular.module('timeApp');
 
 app.controller('mainCtrl', function($scope) {
 	$scope.name = "Ann";
+	// $scope.john = "John";
+	// $scope.jane = "Jane";
 });
--- js/timeDirective.js
+++ js/timeDirective.js
@@ -4,9 +4,23 @@ app.directive('showTime', function() {
 	return {
 		restrict: 'E',
 		template: '<div>The current time is {{time}}</div>',
+		// template: '<div>The current time is {{time}} {{matchmaker}} {{personName}}' + '<button id="btn">Meet</button>' + '</div>',
 		link: function(scope, element, attrs) {
 			var currentTime = new Date();
 			scope.time = currentTime.toString();
+
+			//This element is making the button do something when we click it (I didn't write the function out but it would be in the controller)
+			// element.on('click', function($scope.personName) {
+
+			// });
+		},
+		scope: { //isolate scope (isolated from parents so can do own thing)
+			//'=name' (the =) is two-way binding (like doing ng-model); find thing with same name as what is after = on the html and then matches/links them
+				// matchmaker: '=name'
+			//'@' is taking the value of what is in the html and making an exact copy
+			matchmaker: '@' //string-literal; this looks in html for property w/same name; try and take that value and put in on our scope for us
+			personName: '=name' //two-way binding back to a controller
+			meetFn: '&meet' //expression (didn't write this function out)
 		}
 	}
 });
